e88ef507e47fb86311e1eaf6c58345e055281ff9
Charles Julian Knight <charles@rabidaudio.com>
2015-02-22 05:01:56 -0500

    removed unused crap

--- public/js/site.js
+++ public/js/site.js
@@ -33,20 +33,6 @@ jQuery(document).ready(function($) {
     });
   });
 
-  //$('#nav-main').scrollspy()
-
-  //pretty photo
-  $("a[rel^='prettyPhoto']").prettyPhoto();
-
-  // $('#banner').unslider({
-  //   speed: 500,               //  The speed to animate each slide (in milliseconds)
-  //   delay: 3000,              //  The delay between slide animations (in milliseconds)
-  //   complete: function() {},  //  A function that gets called after every slide animation
-  //   keys: true,               //  Enable keyboard (left, right) arrow shortcuts
-  //   dots: true,               //  Display dot navigation
-  //   fluid: false              //  Support responsive design. May break non-responsive designs
-  // });
-
   //form submission
   $('#signup').submit(function(e){
     //show loader
