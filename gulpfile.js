3980c01c8e1764f72485d97f117914ddf6f7fd8e
Charles Julian Knight <charles@rabidaudio.com>
2015-03-06 21:10:49 -0500

    jshint, jade, etc

--- gulpfile.js
+++ gulpfile.js
@@ -7,58 +7,99 @@ var wrap       = require("gulp-wrap");
 var sass       = require('gulp-sass');
 var uncss      = require('gulp-uncss');
 var csso       = require('gulp-csso');
+var jshint     = require('gulp-jshint');
+var gutil      = require('gulp-util');
+var jade       = require('gulp-jade');
 var del        = require('del');
 var swank      = require('swank');
+var stylish    = require('jshint-stylish');
+
 
 var development = !process.env['PRODUCTION'];
 
 var paths = {
   styles  : 'sass',
-  scripts : 'lib',
+  scripts : 'lib/**/*.js',
+  pages   : 'pages/**/*.jade',
   general : 'src',
   bower   : 'bower_components',
   out     : 'public'
 };
 
 
-gulp.task('clean', function(cb) {
-  del([paths.out+'/*'], cb);
+gulp.task('clean:scripts', function(cb) {
+  del([paths.out+'js/**/*'], cb);
+});
+gulp.task('clean:styles', function(cb) {
+  del([paths.out+'css/**/*'], cb);
+});
+gulp.task('clean:pages', function(cb) {
+  del([paths.out+'/**/*.html'], cb);
+});
+gulp.task('clean:all', function(cb) {
+  del([paths.out+'/**/*'], cb);
+});
+gulp.task('clean:bower', function(cb) {
+  del([paths.out+'vendor/**/*'], cb);
 });
 
-gulp.task('scripts', function() {
-  gulp.src(paths.scripts+'/*.js')
+gulp.task('scripts', ['clean:scripts'], function() {
+  //for all js source files
+  gulp.src(paths.scripts)
+    //modularize each file with `use strict` statements
+    .pipe(wrap('(function () {\n\'use strict\';\n<%= contents %>\n})();'))
+    //run jshint (using .jshintrc)
+    .pipe(jshint())
+    .pipe(jshint.reporter(stylish))
+    .pipe(jshint.reporter('fail'))
+    //combine to one file
     .pipe(concat('site.js'))
-    .pipe(wrap('jQuery(document).ready(function($){<%= contents %>});'))
+    //wrap in a `document.ready` statement
+    .pipe(wrap('jQuery(document).ready(function($){\n<%= contents %>\n});'))
+    //sourcemaps only if in development mode
     .pipe(gulpif(development, sourcemaps.init()))
+    //minify
     .pipe(uglify())
     .pipe(gulpif(development, sourcemaps.write()))
     .pipe(gulp.dest(paths.out+'/js/'));
 });
 
-gulp.task('styles', function () {
+gulp.task('styles', ['clean:styles'], function () {
+    //for all stylesheets
     gulp.src(paths.styles+'/*')
+      //sourcemaps only if in development mode
       .pipe(gulpif(development, sourcemaps.init()))
+      //compile sass
       .pipe(sass())
-      .pipe(uncss({ html: [paths.general+'/index.html'] }))
+      //remove unreferenced styles
+      .pipe(uncss({ html: [paths.out+'/*.html'] }))
+      //minify
       .pipe(csso())
       .pipe(gulpif(development, sourcemaps.write()))
       .pipe(gulp.dest(paths.out+'/css'));
 });
 
-gulp.task('general', function(){
-  gulp.src(paths.general+'/**/*').pipe(gulp.dest(paths.out)); //copy
+gulp.task('pages', ['clean:pages'], function(){
+  gulp.src(paths.pages)
+    .pipe(jade())
+    .pipe(gulp.dest(paths.out));
 });
 
-gulp.task('bower', function(){
-  gulp.src(paths.bower+'/**/*').pipe(gulp.dest(paths.out+'/vendor/')); //copy
+gulp.task('general', function(){
+  gulp.src(paths.general+'/**/*').pipe(gulp.dest(paths.out)); //copy all
+});
+
+gulp.task('bower', ['clean:bower'], function(){
+  gulp.src(paths.bower+'/**/*').pipe(gulp.dest(paths.out+'/vendor/')); //copy all
 });
 
 
 gulp.task('watch', function() {
   gulp.watch(paths.scripts, ['scripts']);
-  gulp.watch(paths.styles, ['styles']);
+  gulp.watch(paths.styles,  ['styles']);
+  gulp.watch(path.pages,    ['pages']);
   gulp.watch(paths.general, ['general']);
-  gulp.watch(paths.bower, ['bower']);
+  gulp.watch(paths.bower,   ['bower']);
 });
 
 gulp.task('serve', function(cb){
@@ -66,10 +107,10 @@ gulp.task('serve', function(cb){
     watch: true,
     path: paths.out
   }, function(err, warn, url){
-    console.log('Server running: '+url);
+    gutil.log('Server running:', url);
   });
 });
 
-gulp.task('build', ['clean', 'scripts', 'styles', 'general', 'bower'])
+gulp.task('build', ['pages', 'scripts', 'styles', 'general', 'bower'])
 
-gulp.task('default', ['build', 'watch', 'serve']);
+gulp.task('default', ['clean:all', 'build', 'watch', 'serve']);
